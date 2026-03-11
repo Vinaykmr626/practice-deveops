f35fd7dc1874d8b3fd3833c86675fd07826f77b8
Ubuntu <ubuntu@ip-172-31-66-240.ec2.internal>
2026-03-11 15:17:34 +0000

    changed welcome message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps project on AWS!"
+    return "Hello Mukesh we have to learn devops man or else very difficult. i have build this via github action !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
